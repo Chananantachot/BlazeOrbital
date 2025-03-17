ccf20180ff46f825ed625508e3a70698ff964149
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-03-17 19:52:27 +0700

    Added .Net Aspire to project solutions

---
+++ AppHost/Program.cs
@@ -0,0 +1,6 @@
+var builder = DistributedApplication.CreateBuilder(args);
+var centralServer = builder.AddProject<Projects.BlazeOrbital_CentralServer>("server");
+builder.AddProject<Projects.BlazeOrbital_ManufacturingHub>("manufacturing")
+        .WithReference(centralServer);
+
+builder.Build().Run();

f8da66c4e2766c3513348aaa3de185b7f33fe040
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-03-19 12:03:44 +0700

    added .env files for AppHost project

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,6 +1,8 @@
+using DotNetEnv;
 var builder = DistributedApplication.CreateBuilder(args);
 var centralServer = builder.AddProject<Projects.BlazeOrbital_CentralServer>("server");
 builder.AddProject<Projects.BlazeOrbital_ManufacturingHub>("manufacturing")
         .WithReference(centralServer);
 
+Env.Load();
 builder.Build().Run();

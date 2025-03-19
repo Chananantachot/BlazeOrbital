32acfc7c2be664dc57b68e841f4e8f3983f60c59
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-03-19 14:32:55 +0700

    removed DotNetEnv

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,8 +1,7 @@
-using DotNetEnv;
+
 var builder = DistributedApplication.CreateBuilder(args);
 var centralServer = builder.AddProject<Projects.BlazeOrbital_CentralServer>("server");
 builder.AddProject<Projects.BlazeOrbital_ManufacturingHub>("manufacturing")
         .WithReference(centralServer);
 
-Env.Load();
 builder.Build().Run();

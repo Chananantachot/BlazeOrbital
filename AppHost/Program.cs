88f7b53ee8a38ea0e033e9168f5a36ca07a778f6
Atip Chananantachot <achananantachot@Atips-MacBook-Pro.local>
2025-03-19 21:26:54 +0700

    added missioncontrol to apphost project

--- AppHost/Program.cs
+++ AppHost/Program.cs
@@ -1,7 +1,16 @@
 
 var builder = DistributedApplication.CreateBuilder(args);
-var centralServer = builder.AddProject<Projects.BlazeOrbital_CentralServer>("server");
-builder.AddProject<Projects.BlazeOrbital_ManufacturingHub>("manufacturing")
+var centralServer = builder.AddProject<Projects.BlazeOrbital_CentralServer>("centralserver");
+var manufacturing = builder.AddProject<Projects.BlazeOrbital_ManufacturingHub>("manufacturing")
         .WithReference(centralServer);
 
+builder.AddNpmApp("missioncontrol", "/Users/achananantachot/Downloads/BlazeOrbital/MissionControl")
+        .WithEndpoint(port: 4200, scheme: "https", env: "PORT")
+        .WithReference(centralServer)
+        .WaitFor(manufacturing)
+        .WithEnvironment("BROWSER", "none") 
+        .WithEnvironment("HTTPS", "true")
+        .WithEnvironment("SSL_CRT_FILE", "server.cert")
+        .WithEnvironment("SSL_KEY_FILE", "server.key");
+        
 builder.Build().Run();
